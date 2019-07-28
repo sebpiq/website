1ad6b088c4189a7d9917554f5bdc94845824d50a
Sebastien Piquemal <sebpiq@gmail.com>
2019-07-28 18:42:17 +0200

    removed url root

---
+++ docs/precache-manifest.b37c02130a8b41046fb5576e8bb97200.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bfdf7f5c26d09ad7713edd4151e9350",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37df79c8dd7d0951025d",
+    "url": "/static/css/main.619419b9.chunk.css"
+  },
+  {
+    "revision": "caddd0be82414e41202f",
+    "url": "/static/js/2.8861adb4.chunk.js"
+  },
+  {
+    "revision": "37df79c8dd7d0951025d",
+    "url": "/static/js/main.5dee7eba.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
