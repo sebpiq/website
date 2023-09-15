f79ae5a5f8c5fbde5538ede24b3aca48448f5cdf
Sébastien Piquemal <sebpiq@protonmail.com>
2023-09-15 11:34:29 +0200

    added more button for articles tile

--- src/TileArticles.js
+++ src/TileArticles.js
@@ -4,6 +4,8 @@ import Link from './Link'
 import './Tile.css'
 import './TileArticles.css'
 
+const ARTICLES_COUNT = 3
+
 export default function(props) {
     return (
         <div className="Tile TileArticles">
@@ -11,17 +13,24 @@ export default function(props) {
                 <span>Articles and posts</span>
             </div>
             <div className="Tile__body">
-                {articles.slice(0, 4).map((article, i) => 
-                <div className="TileArticles__entry"  key={article.title}>
-                    {article.externalUrl ? 
-                        <a href={article.externalUrl} target="_blank" rel="noreferrer noopener">
-                            <span>{article.title}</span>
-                        </a> :
-                        <Link href={article.url}>
-                            <span>{article.title}</span>
-                        </Link>}
-                </div>
+                {articles.slice(0, ARTICLES_COUNT).map((article, i) => 
+                    <div className="TileArticles__entry"  key={article.title}>
+                        {article.externalUrl ? 
+                            <a href={article.externalUrl} target="_blank" rel="noreferrer noopener">
+                                <span>{article.title}</span>
+                            </a> :
+                            <Link href={article.url}>
+                                <span>{article.title}</span>
+                            </Link>}
+                    </div>
                 )}
+
+                <div className="TileArticles__entry">
+                    <Link href="/articles/">
+                        <span>+ MORE</span>
+                    </Link>
+                </div>
+
             </div>
         </div>
     )
